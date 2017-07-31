bb81975344830e3711349b7fee6dd0599e42bab9
Ali Ahmed <a.ahmed@streaml.io>
2017-07-31 10:41:27 -0700

    Init Commit
    Signed-off-by: Ali Ahmed <a.ahmed@streaml.io>

---
+++ README.md
@@ -0,0 +1,7 @@
+TODO Heron version 14.9
+TODO Pular version 1.18
+
+brew tap aahmed-se/homebrew-heron
+
+* Install bookkeeper
+brew install aahmed-se/heron/bookkeepers
---
+++ bookkeeper.rb
@@ -0,0 +1,44 @@
+class Bookkeeper < Formula
+  desc "BookKeeper is a replicated log service which can be used to build replicated state machines"
+  homepage "http://bookkeeper.apache.org/"
+  url "http://www.trieuvan.com/apache/bookkeeper/bookkeeper-4.4.0/bookkeeper-server-4.4.0-bin.tar.gz"
+  sha256 "2ec1f34ae1f0be4ee54a9d986bbc7ff75bed831a0b47ec8e1cd90c82afe02951"
+  version "4.4.0"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script Dir["#{libexec}/bin/bookkeeper"]
+  end
+  
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <false/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/bookkeeper</string>
+          <string>localbookie</string>
+          <string>10</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/bookkeeper.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/bookkeeper.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+end
