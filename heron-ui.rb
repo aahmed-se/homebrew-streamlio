c30161735cb66196529a2c2f74694a4593f0f8b5
Ali Ahmed <a.ahmed@streaml.io>
2017-08-11 01:50:49 -0700

    Add heron tools porjects

---
+++ heron-ui.rb
@@ -0,0 +1,43 @@
+class HeronUI < Formula
+  desc "A realtime, distributed, fault-tolerant stream processing engine from Twitter"
+  homepage "https://twitter.github.io/heron/"
+  url "https://github.com/twitter/heron/releases/download/0.15.0/heron-tools-0.15.0-darwin.tar.gz"
+  sha256 "020f29517f5adffddd56f0462c024a6cb9ac662057d8236e26e238bc71f73c59"
+  version "0.15"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script Dir["#{libexec}/bin/heron-ui"]
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <false/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/heron-ui</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/heron-ui.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/heron-ui.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+end
