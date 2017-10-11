1d58ccf86dd4e41adef4239c26e99bf18e594205
Ali Ahmed <a.ahmed@streaml.io>
2017-10-11 11:46:30 -0700

    Udpate heron client to 0.16.2

--- heron-client.rb
+++ heron-client.rb
@@ -1,9 +1,9 @@
 class HeronClient < Formula
   desc "A realtime, distributed, fault-tolerant stream processing engine from Twitter"
   homepage "https://twitter.github.io/heron/"
-  url "https://github.com/twitter/heron/releases/download/0.15.2/heron-client-0.15.2-darwin.tar.gz"
-  sha256 "4d75cdb994be6a569ab6e971f3ccdd40a3397e42b360919809a06f6f5461e94e"
-  version "0.15.2"
+  url "https://github.com/twitter/heron/releases/download/0.16.2/heron-client-0.16.2-darwin.tar.gz"
+  sha256 "1d1c1d18e902d11a2f5ba99c1afb7e55693a3b5624b70c356be59b50919e3f31"
+  version "0.16.2"
 
   bottle :unneeded
 
