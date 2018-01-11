ab3f91cbc0feb729c61dc8798f2811a3db9d1e76
Ali Ahmed <a.ahmed@streaml.io>
2018-01-10 21:45:52 -0800

    Fix Plist

--- heron.rb
+++ heron.rb
@@ -31,7 +31,7 @@ class Heron < Formula
     EOS
   end
 
-  def heorn; <<-EOS
+  def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
@@ -57,7 +57,7 @@ class Heron < Formula
     EOS
   end
 
-  def heron_ui_plist; <<-EOS
+  def heron_ui_plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
